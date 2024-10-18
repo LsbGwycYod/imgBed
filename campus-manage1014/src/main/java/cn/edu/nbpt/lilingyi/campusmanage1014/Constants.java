170f476fbf3dbea6809f72ded70dfd78729c3804
LsbGwycYod <2227397951@qq.com>
2024-10-18 16:49:13 +0800

    Delete campus-manage1014 directory

--- campus-manage1014/src/main/java/cn/edu/nbpt/lilingyi/campusmanage1014/Constants.java
+++
@@ -1,5 +0,0 @@
-package cn.edu.nbpt.lilingyi.campusmanage1014;
-
-public class Constants {
-    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
-}
--- campus-manage1014/src/main/java/cn/edu/nbpt/lilingyi/campusmanage1014/pojo/enums/GenderEnum.java
+++
@@ -1,6 +0,0 @@
-package cn.edu.nbpt.lilingyi.campusmanage1014.pojo.enums;
-
-public enum GenderEnum {
-    MALE,
-    FEMALE
-}
