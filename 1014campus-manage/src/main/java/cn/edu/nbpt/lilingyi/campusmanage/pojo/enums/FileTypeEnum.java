4f979480421c49e27dd16f3f4f4b65b57a112586
LsbGwycYod <2227397951@qq.com>
2024-12-10 19:08:31 +0800

    Delete 1014campus-manage directory

--- 1014campus-manage/src/main/java/cn/edu/nbpt/lilingyi/campusmanage/pojo/enums/FileTypeEnum.java
+++
@@ -1,5 +0,0 @@
-package cn.edu.nbpt.lilingyi.campusmanage.pojo.enums;
-
-public enum FileTypeEnum {
-    DOCUMENT,IMG, VIDEO,AUDIO
-}
--- 1014campus-manage/src/main/java/cn/edu/nbpt/lilingyi/campusmanage/pojo/enums/GenderEnum.java
+++
@@ -1,6 +0,0 @@
-package cn.edu.nbpt.lilingyi.campusmanage.pojo.enums;
-
-public enum GenderEnum {
-    MALE,
-    FEMALE
-}
